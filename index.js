41e4c79c873baa6c4e859fb9617656f877938bc2
bekah <elisaalvarez211@gmail.com>
2020-05-20 16:59:43 -0700

    finished constroctor functions

--- index.js
+++ index.js
@@ -39,10 +39,28 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
-}
-
+function Person(name, age) {
+this.name= name;
+this.age =age;
+this.stomach = [];
+};
+Person.prototype.eat = function (edible){
+  if (this.stomach.length<10) {
+  this.stomach.push(edible)
+} 
+};
+Person.prototype.poop = function(empty){
+  this.stomach = [];
+};
+Person.prototype.toString= function(){
+  return `${this.name}, ${this.age}`
+};
+const humanOne= new Person ("Noah",21);
+humanOne.eat ("Chicken");
+humanOne.eat("Pizza");
+console.log(humanOne.stomach);
+humanOne.poop();
+console.log(humanOne.stomach)
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -50,27 +68,64 @@ function Person() {
         + should initialize with an `tank` at 0
         + should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
+        
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+    
+        - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
+      
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+ this.tank = 0;
+ this.odometer =0;
+ this.model = model;
+ this.milesPerGallon = milesPerGallon;
+};
+Car.prototype.fill = function (gallons){
+return this.tank = this.tank + gallons;
+  
 
-}
+};
+Car.prototype.drive = function (distance){
+  this. odometer = this.odometer + distance;
+  
+  return this.tank = distance / this.milesPerGallon;
+};
+
+
+const model =new Car ("Bat-mobile",20);
+model.fill(5)
+model.drive(10);
+console.log(this.tank);
 
 /*
   TASK 3
-    - Write a Baby constructor subclassing Person.
+    - Write a Baby constructor subclassing Person 
+    
     - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+    
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
-        + Should return a string "Playing with x", x being the favorite toy.
+     
+    + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+Baby.prototype= Object.create(Person.prototype)
 
+function Baby(name, age, favoriteToy) {
+ this.name = name;
+ this.age = age;
+ this.favoriteToy=favoriteToy;
+
+Baby.prototype.play = function (){
+  return `Playing with ${favoriteToy}!`
 }
+}
+const myLife =new Baby ("Elisa",25, "video games");
+
+console.log(myLife. play());
 
 /* 
   TASK 4
